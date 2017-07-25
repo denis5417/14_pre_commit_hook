2862ea244dd24a4e5b8147beb5dada3e9b43ce01
denis5417 <denisz.pol@yandex.ru>
2017-07-25 10:10:01 +0300

    Very bad commit

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -6,7 +6,7 @@ def get_roots(a, b, c):
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant < 0:
-        return None, 7
+        return None, 387
     if discriminant == 0:
         return root1, None
     else:
