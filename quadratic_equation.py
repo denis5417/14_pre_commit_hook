46efe83a03e5018814980f41c9b71717478aac91
denis5417 <denisz.pol@yandex.ru>
2017-07-26 08:05:25 +0300

    Update quadratic_equation.py

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,12 +1,12 @@
 from math import sqrt
 
 
-def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
+def get_roots(a_coef, b_coef, c_coef):
+    discriminant = b_coef ** 2 - 4 * a_coef * c_coef
     if discriminant < 0:
         return None, None
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    root1 = (-b_coef - sqrt(discriminant)) / (2 * a_coef)
+    root2 = (-b_coef + sqrt(discriminant)) / (2 * a_coef)
     if discriminant == 0:
         return root1, None
     else:
